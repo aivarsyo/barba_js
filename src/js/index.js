c39f7bcee183ca8af01bbd5e1be12ef4e5486787
aivarsyo <aiva0071@stud.kea.dk>
2020-08-27 11:53:28 +0200

    init

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// Bootstrap
+import '../../node_modules/bootstrap';
+
+// CSS
+import "../scss/style.scss";
+
+import gsap from "gsap";
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
+barba.use(barbaCss);
+
+// --- initialize ---
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init(){
+    
+}
+
+// --- *********** ---
+
