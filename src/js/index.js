6bde9ec3d8efc7e3f95b3acb0140b17f9800996c
aivarsyo <aiva0071@stud.kea.dk>
2020-08-28 15:25:16 +0200

    main doesnt overlay another main when pages changed

--- src/js/index.js
+++ src/js/index.js
@@ -6,86 +6,96 @@ import "../scss/style.scss";
 
 import gsap from "gsap";
 import barba from '@barba/core';
-import barbaCss from '@barba/css';
-barba.use(barbaCss);
+import barbaPrefetch from '@barba/prefetch';
+barba.use(barbaPrefetch);
+//import barbaCss from '@barba/css';
+//barba.use(barbaCss);
 
 // --- initialize ---
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init(){
-    pageTransition();
+  navSetup();
+  barbaInit();
 }
 
 // --- *********** ---
 
-function pageTransition(){
+function navSetup(){
 
-    barba.init({
-      // Options
-    
+  const navbarHeight = document.querySelector(".navbar").offsetHeight;
+
+  console.log(navbarHeight)
+
+  document.querySelector("main").style.marginTop = navbarHeight + "px";
+}
+
+const barbaInit = () => {
+				
+      barba.init({
+
+        // Options
+
+        preventRunning: true,
+
       // Transitions
-      transitions: [/* {
-        name: 'expand', //Custom name to be used in CSS classes
-        sync: true, //Enter and leave happens at the same time when true, one after the other when false (false by default)
-        enter: () => {}, //Allows the custom name to show up in these classes (.fade-enter instead of .barba-enter)
-        leave: () => {}, //Allows the custom name to show up in these classes (.fade-leave instead of .barba-leave) 
-        from: { //Use this transition when going from these pages...
-          namespace: [
-            'page',
-            'page2'
-          ]
-        },
-        to: { //...to these pages (The namespace is given as an attribute to the container in the HTML)
-          namespace: [
-            'page',
-            'page2'
-          ]
-        },
-      }, */
-      {
+
+      transitions: [{
+
         name: 'slide',
         sync: true,
   
         leave: (data) => {
-  gsap.to(data.current.container, {
-    duration: 2,
-    x: "100%"
-  })
+
+          return gsap.to(data.current.container, {
+            duration: .5,
+            x: "-100vw",
+            ease: "power1.out",
+            opacity: 0
+          })
         },
   
         enter: (data) => {
-  
-  gsap.set(data.next.container, {
-    x: "-100%"
-  })
-  
-  gsap.to(data.next.container, {
-    duration: 2,
-    x: 0
-  })
+
+          const tml = gsap.timeline();
+
+          tml.set(data.next.container, {
+            y: -data.current.container.offsetHeight,
+            //opacity: 0
+          })
+          
+           tml.from(data.next.container, {
+            duration: .5,
+            x: "100vw",
+            ease: "power1.out",
+            opacity: 0,
+            onComplete:function(){
+              scrollToTop();
+            }
+          })
+
+          tml.to(data.next.container, {
+            opacity: 1
+            
+          })
+
+          tml.set(data.next.container, {
+            y: 0
+            
+          })
+
+          return tml;
         },
         
-      }/* ,
-      {
-        name: 'slide-reverse',
-        sync: true,
-        to: {
-          namespace: [
-            'home'
-          ]
-        },
-        leave: () => {},
-        enter: () => {}
-        //Adding these empty hooks lets the CSS classes use the transition name (slide-reverse-leave, slide-reverse-leave-active, slide-reverse-leave-to, etc.)
-      } */],
-    
-      // Views
-      // views: [{
-      //   namespace: 'home',
-      //   beforeLeave(data) {
-      //     data.current.container.children[0].style.fontSize = "2em";
-      //   }
-      // }]
+      }],
     });
   }
+
+  const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 4);
+    }
+  };
